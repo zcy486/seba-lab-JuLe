254c13d034226ea2fd5d5bd050c96a95a4651bbd
Alexander Maslew <alexander.maslew@tum.de>
2021-11-08 15:39:12 +0200

    Add Login and Register button to homepage

--- jule-frontend/src/App.tsx
+++ jule-frontend/src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import './App.css';
 import {Outlet} from "react-router-dom";
 import {Container} from "@mui/material";
+import {Button} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
       <Container maxWidth="lg">
+        <h1>Welcome to JuLe</h1>
+        <Button variant="outline-primary">Login</Button>{' '}
+        <Button variant="outline-primary">Register</Button>{' '}
         <Outlet/>
       </Container>
     </div>
