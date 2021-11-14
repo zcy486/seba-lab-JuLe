1360d559300d964a88fe24319d9b15bafcbeec9f
Felix Elfering <felix.elfering@tum.de>
2021-11-14 21:37:36 +0100

    add impressum

---
+++ jule-frontend/src/pages/ImpressumPage/ImpressumPage.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styles from "./ImpressumPage.module.css";
+
+const ImpressumPage = () => {
+  return (
+    <div className={styles.impressum}>
+      <h1>
+        Impressum
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Arcu non sodales neque sodales ut etiam sit amet. Varius vel pharetra vel turpis nunc eget lorem
+        dolor. Risus commodo viverra maecenas accumsan. Mauris a diam maecenas sed enim ut. Ullamcorper sit amet risus
+        nullam eget felis. Vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur. Magna fermentum
+        iaculis eu non diam phasellus vestibulum. Dolor sit amet consectetur adipiscing elit ut aliquam. Enim diam
+        vulputate ut pharetra. Pretium nibh ipsum consequat nisl vel pretium. Turpis egestas integer eget aliquet nibh.
+        In metus vulputate eu scelerisque felis imperdiet proin. Semper feugiat nibh sed pulvinar proin gravida
+        hendrerit lectus.
+      </p>
+      <p>
+        Morbi enim nunc faucibus a. Mattis enim ut tellus elementum sagittis vitae et leo. Magna etiam tempor orci eu
+        lobortis elementum nibh. Sit amet dictum sit amet. Sagittis nisl rhoncus mattis rhoncus urna neque viverra.
+        Purus
+        gravida quis blandit turpis cursus in. Tristique nulla aliquet enim tortor at auctor urna nunc. At urna
+        condimentum mattis pellentesque id nibh tortor id aliquet. Curabitur vitae nunc sed velit dignissim. Sapien eget
+        mi proin sed libero. Cursus mattis molestie a iaculis at erat. Sed felis eget velit aliquet sagittis id
+        consectetur. Sed libero enim sed faucibus turpis in. Mauris ultrices eros in cursus turpis massa tincidunt. A
+        cras
+        semper auctor neque vitae tempus.
+      </p>
+      <p>
+        Eu sem integer vitae justo eget magna fermentum iaculis. Sed tempus urna et pharetra pharetra. Accumsan lacus
+        vel
+        facilisis volutpat est velit egestas dui id. Euismod nisi porta lorem mollis aliquam ut porttitor leo. Consequat
+        interdum varius sit amet mattis vulputate enim nulla. Egestas egestas fringilla phasellus faucibus scelerisque
+        eleifend donec pretium. Facilisis leo vel fringilla est ullamcorper eget nulla facilisi. Orci nulla pellentesque
+        dignissim enim sit amet venenatis urna cursus. Ullamcorper malesuada proin libero nunc consequat interdum varius
+        sit amet. Ac turpis egestas maecenas pharetra convallis posuere morbi leo. Eget velit aliquet sagittis id
+        consectetur purus ut.
+      </p>
+      <p>
+        Sapien pellentesque habitant morbi tristique senectus et. Amet nisl suscipit adipiscing bibendum. Et molestie ac
+        feugiat sed lectus vestibulum mattis ullamcorper velit. Eleifend donec pretium vulputate sapien nec sagittis
+        aliquam malesuada. Amet cursus sit amet dictum sit amet justo. Feugiat scelerisque varius morbi enim nunc
+        faucibus
+        a pellentesque. Eget nullam non nisi est sit amet facilisis magna. Ut sem nulla pharetra diam sit. Urna cursus
+        eget nunc scelerisque viverra mauris in aliquam sem. Tristique senectus et netus et malesuada. Purus in massa
+        tempor nec feugiat nisl. Integer vitae justo eget magna fermentum iaculis eu. Sit amet massa vitae tortor. Eget
+        nunc scelerisque viverra mauris in aliquam sem fringilla. Cum sociis natoque penatibus et magnis dis parturient.
+      </p>
+      <p>
+        Vivamus arcu felis bibendum ut tristique et egestas. Nibh nisl condimentum id venenatis a condimentum vitae
+        sapien. Nisi quis eleifend quam adipiscing. Aliquet lectus proin nibh nisl condimentum id. Nisi lacus sed
+        viverra
+        tellus in hac. Id porta nibh venenatis cras sed felis eget velit aliquet. Neque viverra justo nec ultrices dui
+        sapien eget mi. Fames ac turpis egestas sed. Luctus venenatis lectus magna fringilla. Porttitor rhoncus dolor
+        purus non.
+      </p>
+    </div>
+  );
+};
+
+export default ImpressumPage
