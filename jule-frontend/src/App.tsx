fa6fbf5256a8e66ac383f2e4bfce05fa4bfc7dbc
Felix Elfering <felix.elfering@tum.de>
2021-11-15 20:36:27 +0100

    add navigation to app

--- jule-frontend/src/App.tsx
+++ jule-frontend/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import {Outlet} from "react-router-dom";
 import {Container} from "@mui/material";
+import NavigationBar from "./components/NavigationBar/NavigationBar";
 
 function App() {
     return (
         <div>
+            <NavigationBar loggedIn={true}/>
             <Container maxWidth="lg">
                 <Outlet/>
             </Container>
