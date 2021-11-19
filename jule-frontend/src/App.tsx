4aa370d7e7abc11f8c9b459b66ff0fd61055c53a
Felix Elfering <felix.elfering@tum.de>
2021-11-19 11:20:47 +0100

    fix issues from too quick/sloppy merge requests

--- jule-frontend/src/App.tsx
+++ jule-frontend/src/App.tsx
@@ -5,19 +5,15 @@ import {Container} from "@mui/material";
 import Footer from "./components/Footer/Footer";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
 
-function App() {
-    return (
-        <>
-            <NavigationBar loggedIn={true}/>
-            <Container maxWidth="lg">
-                <Outlet/>
-            </Container>
-            <div className={"verticalSpacer"}/>
-            <Footer/>
-        </>
-    );
-}
-
-
+const App = () => (
+    <>
+        <NavigationBar loggedIn={true}/>
+        <Container maxWidth="lg">
+            <Outlet/>
+        </Container>
+        <div className={"verticalSpacer"}/>
+        <Footer/>
+    </>
+);
 
 export default App;
