c6ba66812fb784b98ce81d58687f93ea18f721e3
Robin Geibel <ga27did@mytum.de>
2021-11-23 09:40:23 +0100

    changed app

--- jule-backend/app.py
+++ jule-backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from blueprints.exercise import exercise_routes
+from blueprints.grades import grades_routes
 from db import db_session, init_db
 
 app = Flask(__name__)
@@ -19,6 +20,8 @@ def shutdown_session(exception=None):
 
 # TODO: register more blueprints here
 app.register_blueprint(exercise_routes, url_prefix='/exercise')
+
+app.register_blueprint(grades_routes, url_prefix='/grades')
 
 if __name__ == "__main__":
     app.run(debug=True)
