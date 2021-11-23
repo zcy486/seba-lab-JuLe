3d83233b70b3be194eb0ad18580513d7d372eeef
Robin Geibel <ga27did@mytum.de>
2021-11-23 09:45:57 +0100

    corrected mistake in grades api

--- jule-backend/blueprints/grades.py
+++ jule-backend/blueprints/grades.py
@@ -3,7 +3,7 @@ import os, sys
 
 grades_routes = Blueprint('grades', __name__)
 
-@statistics_routes.route('/<exercise_id>/<student_id>', methods=['GET, POST'])
+@grades_routes.route('/<exercise_id>/<student_id>', methods=['GET, POST'])
 def grade(exercise_id, student_id):
     if request.method == 'GET':
         # TODO: get grade from db
