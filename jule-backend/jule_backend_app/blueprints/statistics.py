dfa9b902716e5d3100d0c0051867860cacaa0727
Robin Geibel <ga27did@mytum.de>
2021-12-07 09:39:13 +0100

    removed post since it was moved to submission

--- jule-backend/jule_backend_app/blueprints/statistics.py
+++ jule-backend/jule_backend_app/blueprints/statistics.py
@@ -9,7 +9,7 @@ from jule_backend_app.models import StatisticType
 textstat.set_lang('de')
 
 
-# calculutae all statistics for one solution
+# calculate all statistics for one solution
 # TODO: add statistics
 # TODO: add statistics typs to data base
 def calculate_statistics(text):
@@ -48,47 +48,13 @@ def get_statistics_types():
         return abort(405)
 
 
-# create or return all available statistics for one student and one exercise
-@statistics_routes.route('/statistics/<student_id>/<exercise_id>/<submission_id>', methods=['POST'])
-def add_statistics(exercise_id, student_id, submission_id):
-    if request.method == 'POST':
-        try:
-            params = request.json
-
-            # get parameters from the request body
-            if params is None:
-                params = request.args
-
-            if params is not None:
-                if 'text' in params:
-                    text = params['text']
-                    student_stats = calculate_statistics(text)
-
-                    # iterate over calculated statistics and add each to db
-                    for stat_title in student_stats:
-                        stat_value, stat_type_id = student_stats[stat_title]
-                        stat = Statistic(statistic_type_id=stat_type_id, exercise_id=exercise_id, student_id=student_id,
-                                         submission_value=stat_value, submission_id=submission_id)
-                        db.session.add(stat)
-                        db.session.commit()
-                else:
-                    return abort(400, "Parameter missinig from request.")
-
-        except Exception as N:
-            print(N)
-            # TODO: make except less general
-            return abort(405)
-    else:
-        return abort(405)
-
-
 @statistics_routes.route('/statistics/<exercise_id>/<student_id>', methods=['GET'])
 def get_statistics(exercise_id, student_id):
     if request.method == 'GET':
         try:
             # TODO: check how return of group by looks like reformat return
             # get statistics from db
-            exercise = Exercise.get(exercise_id)
+            exercise = Exercise.query.get(exercise_id)
             student_stats = Statistic.query.filter_by(exercise_id=exercise_id, student_id=student_id).all()
             peer_stats = db.session.query(func.avg(Statistic.submission_value)).group_by(Statistic.statistic_type_id)
             solution_stats = calculate_statistics(exercise.sample_solution)
