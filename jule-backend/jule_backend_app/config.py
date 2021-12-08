4958bda7e097fe4ce4aa4ab41fb8ebb904954b97
Felix Elfering <felix.elfering@tum.de>
2021-12-08 15:20:23 +0100

    comment out online db

--- jule-backend/jule_backend_app/config.py
+++ jule-backend/jule_backend_app/config.py
@@ -1,6 +1,6 @@
 SQLALCHEMY_DATABASE_URI = "postgresql://localhost/JuLe"  # TODO: change database uri
 # online development DB
-SQLALCHEMY_DATABASE_URI = "postgresql://igbrqirw:M1zug_jh7BGXLUfkpLDrU5pHgE_mL5b5@tai.db.elephantsql.com/igbrqirw"
+# SQLALCHEMY_DATABASE_URI = "postgresql://igbrqirw:M1zug_jh7BGXLUfkpLDrU5pHgE_mL5b5@tai.db.elephantsql.com/igbrqirw"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 JWT_SECRET_KEY="73616cd26965b0cacf7a945bb6e1a82c76006cfd63a9ef08f537ffb35b430622"
